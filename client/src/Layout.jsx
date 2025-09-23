a8b0db71762525ea486171492987cc41610864c1
IT21831386 <it21831386@my.sliit.lk>
2025-09-24 00:46:12 +0530

    implement layout

---
+++ client/src/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navigation from "./components/navigation";
+import Footer from "./components/footer";
+
+const Layout = () => {
+  return (
+    <div data-theme="forest" className="flex flex-col min-h-screen">
+      <Navigation />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
